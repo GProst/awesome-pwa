eaf2d63fc6a72fd0cde1609e14cbee61784e3b2d
GProst <gprostmail@gmail.com>
2017-09-13 00:48:56 +0300

    Remove NotSupported page

--- src/redux/store/genericValidation/browser.js
+++
@@ -1,9 +0,0 @@
-import {push} from 'react-router-redux'
-
-import {Routes} from '../../../routes'
-import {isChrome} from '../../../utils/browserManager'
-
-export const validateBrowser = (store) => {
-  const {pathname} = store.getState().router.location
-  pathname !== Routes.notSupported && !isChrome() && store.dispatch(push(Routes.notSupported))
-}
--- src/redux/store/genericValidation/index.js
+++ src/redux/store/genericValidation/index.js
@@ -1,9 +1,7 @@
 import {validateUser} from './user'
-import {validateBrowser} from './browser'
 
 export function validateStore(store) {
   store.subscribe(() => {
-    validateBrowser(store)
     validateUser(store)
   })
 }
