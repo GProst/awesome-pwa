b20383f406626590178b556d6e7af2498e35dba6
GProst <gprostmail@gmail.com>
2017-09-19 14:32:20 +0300

    Create redux DevTools comp

---
+++ src/redux/DevTools/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {createDevTools} from 'redux-devtools'
+import Inspector from 'redux-devtools-inspector'
+import SliderMonitor from 'redux-slider-monitor'
+import DockMonitor from 'redux-devtools-dock-monitor'
+
+const DevTools = createDevTools(
+  <DockMonitor
+    toggleVisibilityKey='ctrl-h'
+    changePositionKey='ctrl-q'
+    changeMonitorKey='ctrl-m'
+    defaultPosition='right'
+    defaultIsVisible
+  >
+    <Inspector />
+    <SliderMonitor keyboardEnabled />
+  </DockMonitor>
+)
+
+export default DevTools
