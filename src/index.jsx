dc9149892133dacb56edc379dca2a4c0a757597f
GProst <gprostmail@gmail.com>
2018-01-29 13:42:20 +0300

    Refactor index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,40 +3,22 @@ import React from 'react'
 
 import Root from './components/Root'
 
-const moduleProxy = {
-  get hot() {
-    return module.hot
-  },
-  hotAccept(callback) {
-    // unfortunately there is no way to test this method call in unit tests
-    // see this issue: https://github.com/webpack/webpack/issues/5668
-    module.hot.accept('./components/Root', callback)
+if (process.env.NODE_ENV !== 'production') {
+  const {AppContainer} = require('react-hot-loader')
+  const render = Component => {
+    ReactDOM.render(
+      <AppContainer>
+        <Component />
+      </AppContainer>,
+      document.getElementById('root')
+    )
   }
-}
 
-export function init(module) {
-  if (process.env.NODE_ENV !== 'production') {
-    const {AppContainer} = require('react-hot-loader')
-    const render = Component => {
-      ReactDOM.render(
-        <AppContainer>
-          <Component />
-        </AppContainer>,
-        document.getElementById('root')
-      )
-    }
+  render(Root)
 
-    render(Root)
-
-    if (module.hot) {
-      module.hotAccept(() => { render(Root) })
-      return 'develop hot'
-    }
-    return 'develop'
-  } else {
-    ReactDOM.render(<Root />, document.getElementById('root'))
-    return 'production'
+  if (module.hot) {
+    module.hot.accept('./components/Root', () => { render(Root) })
   }
+} else {
+  ReactDOM.render(<Root />, document.getElementById('root'))
 }
-
-init(moduleProxy)
