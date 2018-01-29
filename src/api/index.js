65b9aa1e914561f3375c9aca2cbe585387c3de9c
GProst <gprostmail@gmail.com>
2018-01-30 00:00:54 +0300

    Update api

--- src/api/index.js
+++ src/api/index.js
@@ -3,25 +3,9 @@ class API {
     return new Promise((resolve, reject) => {
       setTimeout(() => { // TODO: real request
         resolve({
-          profile: {
-            name: 'GProst'
-          },
           token: 'asbgrsrvraaece'
         })
       }, 4000)
-    })
-      .catch((err) => {
-        throw err
-      })
-  }
-
-  fetchProfileData() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => { // TODO: real request
-        resolve({
-          name: 'GProst'
-        })
-      }, 2000)
     })
       .catch((err) => {
         throw err
