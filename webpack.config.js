f80fc618040aacc9a1dc012078e5723aebca4f15
GProst <gprostmail@gmail.com>
2018-02-03 14:41:00 +0300

    Fix sourceMaps for production

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ const config = {
 
     isProd ? new webpack.optimize.ModuleConcatenationPlugin() : null,
 
-    isProd ? new UglifyWebpackPlugin() : null
+    isProd ? new UglifyWebpackPlugin({sourceMap: true}) : null
 
   ].filter(Boolean),
   module: {
