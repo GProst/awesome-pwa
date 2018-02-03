379c3303755c7105233c9f2c7d3df55df61ddcab
GProst <gprostmail@gmail.com>
2018-02-03 14:42:53 +0300

    Update setting of NODE_ENV for webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ const config = {
     new webpack.DefinePlugin({
       process: {
         env: {
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+          NODE_ENV: JSON.stringify(isProd ? 'production' : 'development')
         }
       }
     }),
