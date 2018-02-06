2fb803d2d976e3ad4aa3e303d9f9bc14ae4e11b1
GProst <gprostmail@gmail.com>
2018-02-06 08:57:19 +0300

    Update styles for TextInput in AuthForm

--- src/components/pages/Authorization/AuthForm/Inputs/index.jsx
+++ src/components/pages/Authorization/AuthForm/Inputs/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
-import TextField from 'material-ui/TextField'
+import _TextField from 'material-ui/TextField'
 
-export const Container = styled.div`
+const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: stretch;
+`
+
+const TextField = styled(_TextField)`
+  margin-top: 10px!important;
+
+  :first-child {
+    margin-top: 0!important;
+  }
 `
 
 export class Inputs extends React.Component {
@@ -22,6 +30,7 @@ export class Inputs extends React.Component {
           InputLabelProps={{
             shrink: true
           }}
+          helperText=' '
         />
         <TextField
           label='Email'
@@ -29,6 +38,7 @@ export class Inputs extends React.Component {
           InputLabelProps={{
             shrink: true
           }}
+          helperText=' '
         />
         <TextField
           type='password'
@@ -36,6 +46,7 @@ export class Inputs extends React.Component {
           InputLabelProps={{
             shrink: true
           }}
+          helperText=' '
         />
       </Container>
     )
