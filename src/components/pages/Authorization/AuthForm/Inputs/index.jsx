2e221ee8d2131f66e4808970faf7683c4169c9c6
GProst <gprostmail@gmail.com>
2018-02-06 09:57:45 +0300

    Add icons to AuthForm inputs

--- src/components/pages/Authorization/AuthForm/Inputs/index.jsx
+++ src/components/pages/Authorization/AuthForm/Inputs/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import _TextField from 'material-ui/TextField'
+import TextField from 'material-ui/TextField'
+
+import {MaterialIcon} from '../../../../reusable/MaterialIcon'
 
 const Container = styled.div`
   width: 100%;
@@ -10,12 +12,21 @@ const Container = styled.div`
   align-items: stretch;
 `
 
-const TextField = styled(_TextField)`
-  margin-top: 10px!important;
+const InputContainer = styled.label`
+  display: flex;
+  flex-direction: row;
+  justify-content: stretch;
+  align-items: center;
+  margin-top: 10px;
 
   :first-child {
-    margin-top: 0!important;
+    margin-top: 0;
   }
+`
+
+const IconContainer = styled.div`
+  margin-right: 8px;
+  height: 32px;
 `
 
 export class Inputs extends React.Component {
@@ -24,30 +35,48 @@ export class Inputs extends React.Component {
   render() {
     return (
       <Container>
-        <TextField
-          label='Full Name'
-          placeholder='John Doe'
-          InputLabelProps={{
-            shrink: true
-          }}
-          helperText=' '
-        />
-        <TextField
-          label='Email'
-          placeholder='johndoe@example.com'
-          InputLabelProps={{
-            shrink: true
-          }}
-          helperText=' '
-        />
-        <TextField
-          type='password'
-          label='Password'
-          InputLabelProps={{
-            shrink: true
-          }}
-          helperText=' '
-        />
+        <InputContainer>
+          <IconContainer>
+            <MaterialIcon name='AccountBox' />
+          </IconContainer>
+          <TextField
+            label='Full Name'
+            placeholder='John Doe'
+            fullWidth
+            InputLabelProps={{
+              shrink: true
+            }}
+            helperText=' '
+          />
+        </InputContainer>
+        <InputContainer>
+          <IconContainer>
+            <MaterialIcon name='Email' />
+          </IconContainer>
+          <TextField
+            label='Email'
+            placeholder='johndoe@example.com'
+            fullWidth
+            InputLabelProps={{
+              shrink: true
+            }}
+            helperText=' '
+          />
+        </InputContainer>
+        <InputContainer>
+          <IconContainer>
+            <MaterialIcon name='Lock' />
+          </IconContainer>
+          <TextField
+            type='password'
+            label='Password'
+            fullWidth
+            InputLabelProps={{
+              shrink: true
+            }}
+            helperText=' '
+          />
+        </InputContainer>
       </Container>
     )
   }
