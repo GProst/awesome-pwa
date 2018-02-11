e01efa4f56842ae292b76f0122ea16cacbf26ad0
GProst <gprostmail@gmail.com>
2018-02-11 16:32:10 +0300

    Refactor animAuthPage.js

--- src/components/pages/Authorization/animAuthPage.js
+++ src/components/pages/Authorization/animAuthPage.js
@@ -2,4 +2,4 @@ import Easing from 'animated/lib/Easing'
 
 export const duration = 250
 
-export const formContainerEasing = Easing.bezier(0, 0, 0.58, 1) // ease-out
+export const easeOut = Easing.bezier(0, 0, 0.58, 1)
