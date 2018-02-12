28e2a883c40af1883456cfd67c0a85538611fb05
GProst <gprostmail@gmail.com>
2018-02-12 11:45:18 +0300

    Add animation for FormContainer

--- src/components/pages/Authorization/AuthForm/Form/FormContainer.jsx
+++ src/components/pages/Authorization/AuthForm/Form/FormContainer.jsx
@@ -1,38 +1,70 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import Animated from 'animated/lib/targets/react-dom'
 
 import {AUTH_TYPE} from '../../stateAuthPage'
+import {easeIn, easeOut} from '../../../../../constants/animation'
+import {baseDuration} from '../../animAuthPage'
 
-const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
+const duration = baseDuration / 2
 
 export class FormContainer extends React.Component {
   static displayName = 'FormContainer'
 
   static propTypes = {
     children: PropTypes.node.isRequired,
+    onceMounted: PropTypes.bool.isRequired,
     authType: PropTypes.oneOf(Object.values(AUTH_TYPE)).isRequired,
     formType: PropTypes.oneOf(Object.values(AUTH_TYPE)).isRequired,
     toggleView: PropTypes.func.isRequired
   }
 
+  state = {
+    animClipPath: new Animated.Value(1)
+  }
+
+  componentWillMount() {
+    if (this.props.onceMounted) {
+      const {animClipPath} = this.state
+      animClipPath.setValue(0)
+      Animated.timing(animClipPath, {toValue: 1, duration, easing: easeIn})
+        .start()
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
-    if (this.props.formType !== nextProps.authType) {
-      this.props.toggleView()
+    const {formType, authType} = this.props
+    if (authType !== nextProps.authType) {
+      if (formType !== nextProps.authType) { // if we need to switch to another authType
+        Animated.timing(this.state.animClipPath, {toValue: 0, duration, easing: easeOut})
+          .start(({finished}) => {
+            if (finished) {
+              this.props.toggleView()
+            }
+          })
+      } else { // if switch was cancelled and we need to return to the state of current authType
+        Animated.timing(this.state.animClipPath, {toValue: 1, duration, easing: easeOut}).start()
+      }
     }
   }
 
   render() {
     return (
-      <Container>
+      <Animated.div
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          clipPath: this.state.animClipPath.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['circle(0px at center)', 'circle(240px at center)']
+          })
+        }}
+      >
         {this.props.children}
-      </Container>
+      </Animated.div>
     )
   }
 }
