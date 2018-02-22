9f8784bf826a945bbd3d4fc3bbd1b8f2435124a9
GProst <gprostmail@gmail.com>
2018-02-22 23:24:26 +0300

    Update validation

--- src/components/pages/Authentication/AuthForm/Form/validation.js
+++ src/components/pages/Authentication/AuthForm/Form/validation.js
@@ -5,35 +5,72 @@ const defaultValidationState = {
 
 export const validateEmail = value => {
   const validation = {...defaultValidationState}
-  if (value.length === 0) {
-    validation.status = false
-    validation.error = 'Please provide your email'
-  } else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/.test(value)) {
-    validation.status = false
-    validation.error = 'Email is not correct'
+  switch (true) {
+    case value.length === 0: {
+      validation.status = false
+      validation.error = 'Please provide your email'
+      break
+    }
+    case !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/.test(value): {
+      validation.status = false
+      validation.error = 'Email is not correct'
+      break
+    }
   }
   return validation
 }
 
 export const validateName = value => {
   const validation = {...defaultValidationState}
-  if (value.length === 0) {
-    validation.status = false
-    validation.error = 'Please provide your name'
-  } else if (value.length > 200) {
-    validation.status = false
-    validation.error = 'Name is too long'
+  switch (true) {
+    case value.length === 0: {
+      validation.status = false
+      validation.error = 'Please provide your name'
+      break
+    }
+    case !/^\w+$/.test(value): {
+      validation.status = false
+      validation.error = 'Username must contain only letters, numbers and underscores'
+      break
+    }
+    case /^_*[0-9]*$/.test(value): {
+      validation.status = false
+      validation.error = 'Username must contain letters'
+      break
+    }
+    case value.length > 200: {
+      validation.status = false
+      validation.error = 'Name is too long'
+      break
+    }
   }
   return validation
 }
 
 export const validateNewPassword = value => {
   const validation = {...defaultValidationState}
-  if (value.length === 0) {
-    validation.status = false
-    validation.error = 'Please provide your passport'
+  switch (true) {
+    case value.length === 0: {
+      validation.status = false
+      validation.error = 'Please provide your passport'
+      break
+    }
+    case value.length < 8: {
+      validation.status = false
+      validation.error = 'Password must contain at least 8 characters'
+      break
+    }
+    case !/[0-9]/.test(value): {
+      validation.status = false
+      validation.error = 'Password must contain at least one number'
+      break
+    }
+    case /^[0-9]+$/.test(value): {
+      validation.status = false
+      validation.error = 'Password must contain at least one not numeric character'
+      break
+    }
   }
-  // TODO
   return validation
 }
 
