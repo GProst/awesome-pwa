bb1d6f75473f194b9eed8519c9e41e7eeb52b183
GProst <gprostmail@gmail.com>
2018-04-06 12:49:56 +0300

    Fix gzip.sh

--- gzip.sh
+++ gzip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find ./public -type f | while read f; do gzip -9 "$f"; done
-find ./public -type f | while read f; do mv "$f" "$(dirname $f)/`basename $f .gz`"; done
+find ./dist -type f | while read f; do gzip -9 "$f"; done
+find ./dist -type f | while read f; do mv "$f" "$(dirname $f)/`basename $f .gz`"; done
