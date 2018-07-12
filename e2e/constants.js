366bf98adbdae4a282338eeada59badcc6ddf109
GProst <gprostmail@gmail.com>
2018-07-13 00:05:33 +0200

    Create initial E2E tests

---
+++ e2e/constants.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const WINDOW_SIZE = {
+  _320_X_568: {
+    width: 320,
+    height: 568
+  },
+  _400_X_728: {
+    width: 400,
+    height: 728
+  }
+}
+
+// available on BrowserStack
+const RESOLUTION = {
+  _1024x768: {
+    width: 1024,
+    height: 768
+  },
+  _1280x960: {
+    width: 1280,
+    height: 960
+  },
+  _1280x1024: {
+    width: 1280,
+    height: 1024
+  },
+  _1600x1200: {
+    width: 1600,
+    height: 1200
+  },
+  _1920x1080: {
+    width: 1920,
+    height: 1080
+  }
+}
+
+module.exports = {
+  WINDOW_SIZE,
+  RESOLUTION
+}
---
+++ e2e/test.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const swd = require('selenium-webdriver')
+const {setWindowSize} = require('./utils')
+const {WINDOW_SIZE: {_400_X_728}, RESOLUTION: {_1920x1080}} = require('./constants')
+
+const {BROWSERSTACK_USER, BROWSERSTACK_KEY} = process.env
+
+// Input capabilities
+const capabilities = {
+  os: 'OS X',
+  os_version: 'High Sierra',
+  browserName: 'Chrome',
+  browser_version: '67.0',
+  'browserstack.local': 'false',
+  'browserstack.user': BROWSERSTACK_USER,
+  'browserstack.key': BROWSERSTACK_KEY,
+  resolution: `${_1920x1080.width}x${_1920x1080.height}`,
+  chromeOptions: {
+    args: ['--disable-infobars']
+  }
+}
+
+const driver = new swd.Builder()
+  .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+  .withCapabilities(capabilities)
+  .build()
+
+setWindowSize({
+  driver,
+  windowInnerSize: _400_X_728,
+  resolution: _1920x1080
+})
+  .then(() => {
+    driver.get('https://dwgo2lfl43tk4.cloudfront.net/')
+      .then(() => {
+        driver
+          .sleep(3000)
+          .then(() => {
+            driver.quit()
+          })
+      })
+      .catch(() => {
+        driver.quit()
+      })
+  })
+  .catch(() => {
+    driver.quit()
+  })
