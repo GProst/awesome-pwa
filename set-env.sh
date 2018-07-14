e7163da30504a68f0eb8c651080606e987fe35cf
GProst <gprostmail@gmail.com>
2018-07-14 02:24:48 +0200

    Remove set-env.sh file from git

--- set-env.sh
+++ set-env.sh
@@ -3,4 +3,4 @@
 export BROWSERSTACK_USER=germanprostakov1
 export BROWSERSTACK_KEY=AkFBhJNJnYMZpRf3mivv
 
-export E2E_TESTS_PARAMS="--browsers Chrome:67.0 --OSs OS---X:High---Sierra --ids 1 --priorities 1 --types other --windowSizes 400x728"
+export E2E_TESTS_PARAMS="--browsers Chrome:67.0 --OSs OS---X:High---Sierra"
