885f608b7e1ce5d158e0e2c3b838e923dabb163c
GProst <gprostmail@gmail.com>
2018-07-14 02:39:27 +0200

    Remove sensitive data from git

--- set-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export BROWSERSTACK_USER=germanprostakov1
-export BROWSERSTACK_KEY=AkFBhJNJnYMZpRf3mivv
-
-export E2E_TESTS_PARAMS="--browsers Chrome:67.0 --OSs OS---X:High---Sierra"
