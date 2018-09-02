4bfc402f3b919ca6a9047600a028184598d7d6ff
GProst <gprostmail@gmail.com>
2018-09-03 00:55:29 +0200

    Remove aws-aplify libs (for now at least)

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,10 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import Amplify from 'aws-amplify'
-import awsExports from './dynamic-aws-exports'
 
 import './styles'
 import './font'
 
 import {Root} from './components/Root'
-
-Amplify.configure(awsExports)
 
 if (process.env.NODE_ENV !== 'production') {
   const {AppContainer} = require('react-hot-loader')
