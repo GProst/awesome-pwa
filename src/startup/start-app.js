5337146262ea0477ce374cc2a83147ca9d9069e3
GProst <gprostmail@gmail.com>
2018-09-17 03:03:52 +0200

    Add error handler on app start

--- src/startup/start-app.js
+++ src/startup/start-app.js
@@ -1,10 +1,16 @@
+import {logger} from '../utils/logger'
+
 export const startApp = () => {
-  const fragment = document.createDocumentFragment()
-  const vendors = document.createElement('script')
-  vendors.src = '/vendors.js'
-  fragment.appendChild(vendors)
-  const app = document.createElement('script')
-  app.src = '/app.js'
-  fragment.appendChild(app)
-  document.body.appendChild(fragment)
+  try {
+    const fragment = document.createDocumentFragment()
+    const vendors = document.createElement('script')
+    vendors.src = '/vendors.js'
+    fragment.appendChild(vendors)
+    const app = document.createElement('script')
+    app.src = '/app.js'
+    fragment.appendChild(app)
+    document.body.appendChild(fragment)
+  } catch(err) {
+    logger.errorRemote(err, {extra: {message: 'Error in startApp function'}})
+  }
 }
