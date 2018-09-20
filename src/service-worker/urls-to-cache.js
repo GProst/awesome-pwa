b3dfe2716d311b1fe6e725034e2e38b44a08a65e
GProst <gprostmail@gmail.com>
2018-09-21 00:34:24 +0200

    Add comments

--- src/service-worker/urls-to-cache.js
+++ src/service-worker/urls-to-cache.js
@@ -1,11 +1,11 @@
 module.exports = {
-  urlsToCache: [
+  urlsToCache: [ // only resources NEEDED to start app smoothly, other resources can be downloaded and cached later
     '/index.html',
-    '/images/favicon.png',
+    '/images/favicon.png', // TODO: I don't need it here
     '/app.js',
     '/vendors.js',
     '/fonts/roboto-400-latin.woff2',
     '/fonts/roboto-900-latin.woff2',
-    '/manifest.webmanifest'
+    '/manifest.webmanifest' // TODO: I don't need it here, but don't forget to update fetch event handler to cache this guy
   ]
 }
